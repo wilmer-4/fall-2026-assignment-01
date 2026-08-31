1adc0c8341c9ceb122aa3ccf75d978299f674156
wilmer-4 <wmercado@LAPTOP-23L0A1PS.localdomain>
2026-08-31 03:53:32 -0400

    fixed issue

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,18 +1,23 @@
 export function transcribeDNA(dna: string): string {
-  for (let i = 0, i < x, i++) {
-    if dna[0] = "A" {
-      dna[0] = "U"
-    } elif dna[0] = "T" {
-      dna[0] = "A"
-    } elif dna[0] = "C" {
-      dna[0] = "G"
-    } elif dna[0] = "G"{
-      dna[0] = "C"
-    } else
+  let rna = "" // rna is an empty string, we will use the for loop to give it values.
+  for (let i = 0; i < dna.length; i++) {
+    const nucleotide = dna[i]; //assign nucleotide to be the current index
+   if (nucleotide === "A") {
+    rna += "U"
+   } else if (nucleotide === "T") {
+    rna += "A"
+   } else if (nucleotide === "C") {
+    rna += "G"
+   } else if (nucleotide === "G") {
+    rna += "C" //use the for loop to add the corresponding characeters to dna
+   } else {
+    throw new Error("There was an invalid nucleotide input!") //if an invalid nucelotide entered, this error will popup
+   }
+  
       
     
   }
   
-
+return rna;
 
 }

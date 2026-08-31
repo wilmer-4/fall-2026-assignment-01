ce61a9f030701b74dbd8214e400f3b5c4d3d0a0e
wilmer-4 <wmercado@LAPTOP-23L0A1PS.localdomain>
2026-08-31 03:45:53 -0400

    newest changes

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,17 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const requests = postIds.map(async (id) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/${id}")
+    const data = await response.json() as PostItem;
+
+    return data;
+  });
+  const posts = await Promise.all(requests)
+
+  return posts;
 }
